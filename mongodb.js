4e6425529b2182601646e70054e762d218a5e69e
Phonekham <phonekham.dev@gmail.com>
2019-07-08 15:35:18 +0700

    connect db

---
+++ mongodb.js
@@ -0,0 +1,21 @@
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+mongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+    // console.log("connected");
+    const db = client.db(databaseName);
+    db.collection("users").insertOne({
+      name: "Phonekham",
+      age: 24
+    });
+  }
+);
