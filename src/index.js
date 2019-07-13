525540889e5e5e9d0246e4dec029e251189557ee
Phonekham <phonekham.dev@gmail.com>
2019-07-13 15:45:02 +0700

    3. Validating File Uploads

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const port = process.env.PORT || 3000;
 
 const multer = require("multer");
 const upload = multer({
-  dest: "images"
+  dest: "images",
+  limits: {
+    fileSize: 1000000 //1MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload word"));
+    }
+    cb(undefined, true);
+  }
 });
 app.post("/upload", upload.single("upload"), (req, res) => {
   res.send();
